597963c5b4de76aa1bfc0b14b80a1f3d3a833ff0
ARUEXP <Ardhendu.tech@gmail.com>
2024-09-01 10:19:46 +0530

    1

--- script.js
+++ script.js
@@ -100,6 +100,22 @@ function loadDataFromURL() {
   }
 }
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js').then(
+      function (registration) {
+        console.log(
+          'Service Worker registered with scope:',
+          registration.scope
+        );
+      },
+      function (error) {
+        console.log('Service Worker registration failed:', error);
+      }
+    );
+  });
+}
+
 // Load data when the page loads
 window.onload = loadDataFromURL;
 
--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,7 @@
-const CACHE_NAME = 'anime-tracker-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/styles.css', // Replace with your CSS file path
-  '/manifest.json',
-  '/icons/icon-192x192.png',
-  '/icons/icon-512x512.png',
-];
-
-// Install Service Worker
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+  console.log('Service Worker installed.');
 });
 
-// Fetch Resources from Cache
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  // For now, it does nothing
 });
